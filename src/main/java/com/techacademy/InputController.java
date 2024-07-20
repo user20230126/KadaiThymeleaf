6b721a0c62479d4aa1f5e0426cdf0787aa3ed3bd
user20230126 <1228mon0224wed@gmail.com>
2024-07-20 23:45:04 +0900

    課題で修正したファイルをプッシュ

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -3,6 +3,8 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
@@ -11,6 +13,12 @@ public class InputController {
     public String getInput(Model model) {
         // input.htmlに画面遷移
         return "input";
+    }
+    
+    @PostMapping("/input{val}/")
+    public String postInput(@RequestParam("val") String val, Model model) {
+        model.addAttribute("val", val);
+        return "input{val}";
     }
     
 }
--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -2,18 +2,11 @@ package com.techacademy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OutputController {
-    
-    @GetMapping("output")
-    public String getOutput() {
-        // output.htmlに画面遷移
-        return "output";
-    }
     
     @PostMapping("/output")
     public String postOutput(@RequestParam("val") String val, Model model) {
@@ -21,12 +14,6 @@ public class OutputController {
         model.addAttribute("val", val);
         // output.htmlに画面遷移
         return "output";
-    }
-    
-    @PostMapping("/input{val}/")
-    public String postInput(@RequestParam("val") String val, Model model) {
-        model.addAttribute("val", val);
-        return "input{val}";
     }
     
 }
